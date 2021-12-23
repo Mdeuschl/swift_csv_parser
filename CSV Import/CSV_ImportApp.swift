9a920688bbdd7f1cb166b87bedaea2f706086e09
Matthias Deuschl <iOS_Apps@deuschl.net>
2021-12-23 17:24:39 +0100

    Initial Commit

---
+++ CSV Import/CSV_ImportApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSV_ImportApp.swift
+//  CSV Import
+//
+//  Created by Matthias Deuschl on 23.12.21.
+//
+
+import SwiftUI
+
+@main
+struct CSV_ImportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
