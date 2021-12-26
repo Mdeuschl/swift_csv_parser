db9c74b008d0a6b2e2479a148557be2193586798
Matthias Deuschl <iOS_Apps@deuschl.net>
2021-12-26 09:05:29 +0100

    Refactor sample data into testing ContentView file

--- CSV Import/ParseCSV.swift
+++ CSV Import/ParseCSV.swift
@@ -7,59 +7,6 @@
 
 import Foundation
 
-let samples = [
-"Regular,csv,file\ra,b,c\r\n1,2,3",
-"Emoji,special,characters\r🥰,½,c\r\n1,&quot;,3\n4,5,ʤ",
-"Different New Lines:\r(r)\n(n)\r\n(rn)",
-"Empty,line,after,(r)\r",
-"Empty,line,after,(n)\n",
-"Empty,line,after,(rn)\r\n",
-"Two,empty,lines,after,(rn)\r\n\r\n",
-"""
-Quoted,second,field
-ha ha ha,\"ha \"\"ha\"\" ha\","ha,ha,ha"
-ha,"ha",ha
-""",
-"""
-
-\"Empty line above\"
-""",
-"""
-first,last,address,city,zip
-John,Doe,120 any st.,"Anytown, WW",08123
-""", //4 extra empty field aufter Anytown…
-"""
-a,b
-1,"ha
-""ha""
-ha"
-3,4
-""", //5 okay.
-"""
-a,b,c
-1,"",""
-2,3,4
-""", //6 no new line after empty fields
-"""
-key,val
-1,"{""type"": ""Point"", ""coordinates"": [102.0, 0.5]}"
-2,"{""type"": ""Point"", ""coordinates"": [99.8, -1.5]}"
-""", //7 okay
-"""
-1,2,3,4,5
-"1",two,"III","",""
-,,,….,.….
-""", //8 one empty field too many in second record.
-"""
-"Format Error 1","Unterminated Quote
-""", //9 extraneuos empty field
-"""
-Format Error,Quote inside "Field"
-#2 "🤯", "leading space and unterminated Quote
-""" //10
-]
-
-//let sampleData = "\"a\";"
 
 struct LineOfFileds: Hashable, Identifiable {
     var fields: Array<Substring>
